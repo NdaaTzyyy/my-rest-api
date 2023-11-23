db4f40744eb232b077a87e9b25c3431c9bbd3a9e
NdaaTzyyy <96452047+NdaaTzyyy@users.noreply.github.com>
2023-11-23 22:53:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const limiter = rateLimit({
 app.use(limiter);
 
 //const path = require('path');
-app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'assets')));
 
 
